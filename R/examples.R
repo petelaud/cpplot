7911651ee097ddc01d4e76e05335ebe3950ebb0a
Pete Laud <p.j.laud@shef.ac.uk>
2025-01-29 11:43:47 +0000

    add code to produce example CIs for inclusion in manuscript

---
+++ R/examples.R
@@ -0,0 +1,35 @@
+if (FALSE) {
+x <- c(1, 1, 7, 12)
+
+# Example CIs for RD
+round(rbind(
+  pairbinci(x = x, contrast = "RD", method_RD = "Score", bcf = TRUE, skew = TRUE)$estimates[,1:3], # SCAS-bc
+  pairbinci(x = x, contrast = "RD", method_RD = "Score", bcf = FALSE, skew = TRUE)$estimates[,1:3], # SCAS(no bc)
+  pairbinci(x = x, contrast = "RD", method_RD = "Score", bcf = FALSE, skew = FALSE)$estimates[,1:3], # AS
+  pairbinci(x = x, contrast = "RD", method_RD = "MOVER_newc", moverbase = 'jeff')$estimates, # MOVER-NJ
+  pairbinci(x = x, contrast = "RD", method_RD = "MOVER_newc", moverbase = 'wilson')$estimates, # MOVER-W
+  pairbinci(x = x, contrast = "RD", method_RD = "MOVER", moverbase = 'wilson')$estimates, # MOVER-W
+  pairbinci(x = x, contrast = "RD", method_RD = "BP")$estimates, # BP
+  pairbinci(x = x, contrast = "RD", method_RD = "Score", bcf = TRUE, skew = TRUE, cc=0.125)$estimates[,1:3], # SCAS-cc
+  pairbinci(x = x, contrast = "RD", method_RD = "Score", bcf = TRUE, skew = TRUE, cc=0.5)$estimates[,1:3], # SCAS-cc
+  pairbinci(x = x, contrast = "RD", method_RD = "MOVER_newc", moverbase = 'jeff', cc=0.125)$estimates, # MOVER-NJcc
+  pairbinci(x = x, contrast = "RD", method_RD = "MOVER_newc", moverbase = 'jeff', cc=0.5)$estimates # MOVER-NJcc
+), 3)[, c(1, 3)]
+
+# Example CIs for RR
+round(rbind(
+  pairbinci(x = x, contrast = "RR", method_RR = "Score", bcf = TRUE, skew = TRUE)$estimates[,1:3], # SCAS-bc
+  pairbinci(x = x, contrast = "RR", method_RR = "Score", bcf = FALSE, skew = TRUE)$estimates[,1:3], # SCAS(no bc)
+  pairbinci(x = x, contrast = "RR", method_RR = "Score", bcf = FALSE, skew = FALSE)$estimates[,1:3], # AS
+  pairbinci(x = x, contrast = "RR", method_RR = "MOVER_newc", moverbase = 'jeff')$estimates, # MOVER-NJ
+  pairbinci(x = x, contrast = "RR", method_RR = "MOVER_newc", moverbase = 'wilson')$estimates, # MOVER-W
+  pairbinci(x = x, contrast = "RR", method_RR = "MOVER", moverbase = 'wilson')$estimates, # MOVER-W
+  pairbinci(x = x, contrast = "RR", method_RR = "BP", moverbase = 'wilson')$estimates, # BP
+  pairbinci(x = x, contrast = "RR", method_RR = "Score", bcf = TRUE, skew = TRUE, cc=0.125)$estimates[,1:3], # SCAS-cc
+  pairbinci(x = x, contrast = "RR", method_RR = "Score", bcf = TRUE, skew = TRUE, cc=0.5)$estimates[,1:3], # SCAS-cc
+  pairbinci(x = x, contrast = "RR", method_RR = "MOVER_newc", moverbase = 'jeff', cc=0.125)$estimates, # MOVER-NJcc
+  pairbinci(x = x, contrast = "RR", method_RR = "MOVER_newc", moverbase = 'jeff', cc=0.5)$estimates # MOVER-NJcc
+), 3)[, c(1, 3)]
+
+
+}
